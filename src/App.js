c060138a521f366da920769fe034d4d6ce2da396
U.A. Yanık <ates.yanik@hva.nl>
2021-09-27 00:20:36 +0200

    Nav menu and mobile sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router} from 'react-router-dom' 
+import { BrowserRouter as Router  } from 'react-router-dom' 
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Sidebar/>
       <Navbar/>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements.js'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements.js'
+import {FaBars}  from "react-icons/fa";
 
 const Navbar = () => {
     return (
@@ -7,18 +8,28 @@ const Navbar = () => {
             <Nav>
                 <NavbarContainer>
                     <NavLogo to='/'>
-                        SuperBank
+                        Smoothie Finance
                     </NavLogo>
                     <MobileIcon>
                         <FaBars/>
                     </MobileIcon>
                     <NavMenu>
                         <NavItem>
-                            <NavLinks to= 'about'>
-                                About
-                            </NavLinks>
+                            <NavLinks to= 'about'> About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to= 'discover'> Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to= 'services'> Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to= 'signup'> Sign up</NavLinks>
                         </NavItem>
                     </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign in</NavBtnLink>
+                    </NavBtn>
                 </NavbarContainer>
             </Nav>
         </>
