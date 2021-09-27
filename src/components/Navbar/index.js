45c3f9c7d27eaaa18855a9d44d99113dee64ee7e
U.A. Yanık <ates.yanik@hva.nl>
2021-09-27 23:26:21 +0200

    sidebar toggle design n function

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements.js'
 import {FaBars}  from "react-icons/fa";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
     return (
         <>
             <Nav>
@@ -10,7 +10,7 @@ const Navbar = () => {
                     <NavLogo to='/'>
                         Smoothie Finance
                     </NavLogo>
-                    <MobileIcon>
+                    <MobileIcon onClick={toggle}>
                         <FaBars/>
                     </MobileIcon>
                     <NavMenu>
